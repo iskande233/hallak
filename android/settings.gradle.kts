f7dc91e96212bc714dcf9bed50c01f6bf13c1832
iskande233 <iskanderkhantouche688@gmail.com>
2026-05-30 00:26:35 +0100

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,8 +18,7 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    // قمنا بتحديث إصدار الـ Kotlin إلى 2.3.0 لحل مشكلة التوافق
+    id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 }
 
